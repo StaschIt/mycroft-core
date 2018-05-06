e5a4640d49597dfd669c870d92df0f0ce72c01ad
Jonas Stasch <Jonas@localhost.localdomain>
2018-05-06 14:31:16 +0200

    first success with testWAVLoading

--- testWAVLoading.py
+++ testWAVLoading.py
@@ -1,13 +1,30 @@
 from Queue import Queue
 
+import unittest
 import speech_recognition
-from os.path import dirname, join
 from speech_recognition import WavFile, AudioData
 
 from mycroft.client.speech.listener import AudioConsumer, RecognizerLoop
 from mycroft.stt import MycroftSTT
+from os.path import join
+from test.integrationtests.skills.skill_tester import MockSkillsLoader
+from test.integrationtests.skills.discover_tests import IntentTestSequenceMeta
 
-from mycroft.skills.core import MycroftSkill
+
+SKILL_PATH = '/opt/mycroft/skills'
+
+
+class IntentTestSequence(unittest.TestCase):
+    __metaclass__ = IntentTestSequenceMeta
+
+    @classmethod
+    def setUpClass(self):
+        self.loader = MockSkillsLoader(SKILL_PATH)
+        self.emitter = self.loader.load_skills()
+
+    @classmethod
+    def tearDownClass(self):
+        self.loader.unload_skills()
 
 
 class MockRecognizer(object):
@@ -54,16 +71,21 @@ class AudioConsumerTest:
 audioconsumer = AudioConsumerTest()
 audioconsumer.setUp()
 
+loader = MockSkillsLoader(SKILL_PATH)
+emitter = loader.load_skills()
+
 audioconsumer.queue.put(audioconsumer.create_sample_from_test_file('mycroft'))
 
 audioconsumer.consumer.read()
-audioconsumer.queue.put(audioconsumer.create_sample_from_test_file('mycroft'))
+audioconsumer.queue.put(audioconsumer.create_sample_from_test_file('hey_mycroft'))
 audioconsumer.recognizer.set_transcriptions(["record"])
 monitor = {}
 
+
 def callback(m):
     monitor['utterances'] = m.get('utterances')
-    print(m)
+    print('Message', m)
+
 
 audioconsumer.loop.once('recognizer_loop:utterance', callback)
 audioconsumer.consumer.read()

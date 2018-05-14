47a82f044553373e0ee5262d9f6458c0c94c5676
Jonas Stasch <Jonas@localhost.localdomain>
2018-05-14 14:19:50 +0200

    edit distance added

--- evaluationSTT.py
+++ evaluationSTT.py
@@ -8,7 +8,7 @@ from speech_recognition import WavFile, AudioData
 from mycroft.client.speech.listener import AudioConsumer, RecognizerLoop
 from mycroft.stt import MycroftSTT
 from os.path import join
-
+import editdistance
 
 speechdata_path = sys.argv[1]
 
@@ -48,7 +48,7 @@ def create_utterances_from_wavfiles(directory):
 
     for wavfile in os.listdir(directory):
         def callback(m):
-            utterances[wavfile.replace('.wav', '')] = m.get('utterances')
+            utterances[wavfile.replace('.wav', '')] = m.get('utterances')[0].upper()
 
         loop.on('recognizer_loop:utterance', callback)
 
@@ -65,9 +65,19 @@ def create_transcriptions_from_file(prompt_file):
 
     for line in lines:
         split = line.split('/')
-        transcriptions[split[-1].split(' ', 1)[0]] = split[-1].split(' ', 1)[1]
+        transcriptions[split[-1].split(' ', 1)[0]] = split[-1].split(' ', 1)[1].decode('unicode-escape')
 
     return transcriptions
+
+
+def total_edit_distance(dic1, dic2):
+    distance = 0
+    for key in dic1:
+        # if editdistance.eval(dic1[key], dic2[key]) > 0:
+        #     print('Mistake! Understood: ' + dic1[key] + ' but was: ' + dic2[key])
+        distance += editdistance.eval(dic1[key], dic2[key])
+
+    return distance
 
 
 # Audio files in voxforge/*/wav
@@ -75,5 +85,9 @@ def create_transcriptions_from_file(prompt_file):
 
 utterances_dic = create_utterances_from_wavfiles(join(speechdata_path, 'wav'))
 transcriptions_dic = create_transcriptions_from_file(join(speechdata_path, 'etc', 'PROMPTS'))
+
+print('distance', total_edit_distance(utterances_dic, transcriptions_dic))
+
+# print (utterances_dic, transcriptions_dic)
 
 print('success')
